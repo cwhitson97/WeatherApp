768276662f0ac5c985a895a3a8b788236fc0b8a4
Courtney Whitson <clw229@txstate.edu>
2020-10-30 15:24:11 -0500

    Linked kv files to main python file and when buttons are pressed they transition to proper kv files

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-#Main page of the application
-#presents users with options to create an account or sign in to an existing account
-#goes alongside main.kv file
+# Main page of the application
+# presents users with options to create an account or sign in to an existing account
+# goes alongside main.kv file
+
 
 import kivy
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
+from kivy.lang import Builder
 
-class MainPage(Widget):
+Builder.load_file('main.kv')
+Builder.load_file('new.kv')
+Builder.load_file('existing.kv')
+
+
+class NewPage(Screen):
     pass
+
+
+class MainPage(Screen):
+    pass
+
+
+class ExistingPage(Screen):
+    pass
+
+
+screen = ScreenManager()
+screen.add_widget(MainPage(name='menu'))
+screen.add_widget(NewPage(name='new'))
+screen.add_widget(ExistingPage(name='existing'))
+
 
 class MainApp(App):
     def build(self):
-        self.title= 'Weather App'
-        return MainPage()
+        return screen
+
 
 if __name__ == '__main__':
     MainApp().run()
